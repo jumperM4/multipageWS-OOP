339f860587f2fd12c2812a18ec28400c25bf2123
jumperM4 <pma1994ars@yandex.ru>
2023-06-01 20:42:32 +0300

    try catch for the 2nd page

--- src/js/modules/sliders/slider.js
+++ src/js/modules/sliders/slider.js
@@ -9,13 +9,15 @@ export default class Slider {
     autoplay,
   } = {}) {
     this.container = document.querySelector(container);
-    this.slides = this.container.children;
+    try {
+      this.slides = this.container.children;
+    } catch (e) {}
     this.btns = document.querySelectorAll(btns);
     this.next = document.querySelector(next);
     this.prev = document.querySelector(prev);
     this.activeClass = activeClass;
     this.animate = animate;
-    this.autoplay = autoplay; 
+    this.autoplay = autoplay;
     this.slideIndex = 1;
   }
 }
