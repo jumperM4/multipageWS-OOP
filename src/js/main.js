03c1f71c5d90a4422d01bf259ec10f68da5e1b02
jumperM4 <pma1994ars@yandex.ru>
2023-06-03 13:21:28 +0300

    show info btn on 2n page

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import MiniSlider from "./modules/sliders/slider-mini";
 import VideoPlayer from "./modules/playVideo";
 import Difference from "./modules/difference";
 import Form from "./modules/forms";
+import ShowInfo from "./modules/showInfo";
 
 window.addEventListener("DOMContentLoaded", () => {
   const slider = new MainSlider({ container: ".page", btns: ".next" });
@@ -50,4 +51,6 @@ window.addEventListener("DOMContentLoaded", () => {
   new Difference(".officerold", ".officernew", ".officer__card-item").init();
 
   new Form(".form").init();
+
+  new ShowInfo(".plus").init();
 });
