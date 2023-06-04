019d15f38761995c8e3e14a8c852f09274b909c2
jumperM4 <pma1994ars@yandex.ru>
2023-06-04 11:23:27 +0300

    consoleLogs removed and new path-download module

--- src/js/modules/download.js
+++ src/js/modules/download.js
@@ -1,25 +1,25 @@
 export default class Download {
   constructor(triggers) {
     this.btns = document.querySelectorAll(triggers);
-    this.path = "assets/img/mainbg.jpg";
+    this.path = "src/assets/img/text.txt";
   }
 
-    downloadItem(path) {
-        const elementLink = document.createElement("a");
-        elementLink.setAttribute("href", path);
-        elementLink.setAttribute("download", 'nice-picture');
+  downloadItem(path) {
+    const elementLink = document.createElement("a");
+    elementLink.setAttribute("href", path);
+    elementLink.setAttribute("download", "text-file");
 
-        elementLink.style.display = "none";
-        
-        document.body.appendChild(elementLink);
-        elementLink.click();
-        document.body.removeChild(elementLink);
+    elementLink.style.display = "none";
+
+    document.body.appendChild(elementLink);
+    elementLink.click();
+    document.body.removeChild(elementLink);
   }
 
   init() {
     this.btns.forEach((btn) => {
-        btn.addEventListener("click", (e) => {
-          e.stopPropagation();
+      btn.addEventListener("click", (e) => {
+        e.stopPropagation();
         this.downloadItem(this.path);
       });
     });
